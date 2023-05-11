d1f2a405c4f52636586e9e933567c92cd5709473
junkkie <jenkem029@gmail.com>
2023-05-11 17:16:59 +0900

    0511

--- src/App.js
+++ src/App.js
@@ -2,37 +2,27 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './routes/Main';
-import SHH from './img/shhh.png'
 import Projects from './routes/Projects';
 import Outtro from './routes/Outtro';
+import Loading from './routes/Loading';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000)
-  },[])
-
   return (
     <>
-    {loading ?
-    <div className='loading'>
-      <img src={SHH} alt='character' />
-      <span>0%</span>
-      <span>SHHHHHHH!</span>
-    </div>
-    :
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
+        {loading ?
+        <Route path='/' element={<Loading setLoading={setLoading} />} />
+        :
         <Route path='/' element={<Main />} />
+        }
         <Route path='/projects' element={<Projects />} />
         <Route path='/outtro' element={<Outtro />} />
       </Routes>
     </BrowserRouter>
-    }
     </>
 
   );
